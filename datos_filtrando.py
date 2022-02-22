a0dd14cce688e176009e850d19b3c5bf95591e92
Alberto Schmalbach <albert2550@hotmail.com>
2022-02-21 19:02:00 -0500

    Practica 21/02/2022

---
+++ datos_filtrando.py
@@ -0,0 +1,44 @@
+aquarium_creatures = [
+  {"name": "sammy", 
+  "species": "shark", 
+  "tank number": 11, 
+  "type": "fish"},
+
+  {"name": "ashley", 
+  "species": "crab", 
+  "tank number": 25, 
+  "type": "shellfish"},
+
+  {"name": "jo", 
+  "species": "guppy", 
+  "tank number": 18, 
+  "type": "fish"},
+
+  {"name": "jackie", 
+  "species": "lobster", 
+  "tank number": 21, 
+  "type": "shellfish"},
+
+  {"name": "charlie", 
+  "species": "shark", 
+  "tank number": 12, 
+  "type": "fish"},
+
+  {"name": "olly", 
+  "species": "green turtle", 
+  "tank number": 34, 
+  "type": "turtle"}
+]
+
+def main():
+    peces =[pez["name"] for pez in aquarium_creatures if pez["type"]=="fish"]
+    #print(peces)
+
+    number = list(filter(lambda pescadito : pescadito["tank number"]< 20, aquarium_creatures))
+    print(number)
+
+    especie = list(map(lambda pez : pez["species"]== "shark", aquarium_creatures))
+    #print(especie)
+
+if __name__=='__main__':
+    main()
